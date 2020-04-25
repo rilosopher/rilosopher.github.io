8206b88b7828e25d52f4f5e7e19f966a97b2fdc5
rilosopher <64277581+rilosopher@users.noreply.github.com>
2020-04-25 11:11:20 +0530

    created new react app

---
+++ precache-manifest.c33f8a9c0bdb63a359504c0557feb4fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144e34789512d9e73e087d062b2f9a92",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "4c1febad44cbcc8b4630",
+    "url": "/reactapp/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "1555fcbf48e39322a99d",
+    "url": "/reactapp/static/js/2.d75131d5.chunk.js"
+  },
+  {
+    "revision": "4c1febad44cbcc8b4630",
+    "url": "/reactapp/static/js/main.e4a4a24c.chunk.js"
+  },
+  {
+    "revision": "b2b8833a44fa82a1fbc7",
+    "url": "/reactapp/static/js/runtime~main.4142c29b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/reactapp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
